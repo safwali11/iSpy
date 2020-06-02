ccaf0dd1ca070ec0cc5f9bd33fa831223cd4169f
safwali11 <58273745+safwali11@users.noreply.github.com>
2020-06-02 16:39:13 -0700

    Add files via upload

--- SWOF_dataset_build_final.py
+++ SWOF_dataset_build_final.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 import SWOF_functions_complete as SWOF
 import numpy as np
-#%pylab inline
 
 
-# In[ ]:
+# In[2]:
 
 
 
@@ -28,70 +27,59 @@ video, length, width, height, fsp = SWOF.load_video('crash5.mp4')
 frames = SWOF.split_video_frames(video, length, height, width)
 
 
-# In[ ]:
+# In[5]:
 
 
 
 #### 2. GENERATE TRAINING DATASETS IN CLASS LABELS DIRECTORIES
 
 
-# In[5]:
+# In[6]:
 
 
 #directory must be labeled as "../dataset/training/crash"  
 CRASHDIR = '/home/jetson/ENGIN492/data_set/training/crash/'
 
-SWOF_crash_images = SWOF.generate_SWOF_data(DATADIR=CRASHDIR,
-                                            VIDEONAME='crash5',
-                                            frames=frames,
-                                            N=5,
-                                            startframe=68,
-                                            endframe=105)
+SWOF_crash_images = SWOF.generate_SWOF_data(CRASHDIR, 'crash5',
+                                            frames, N=5,
+                                            startframe=68, endframe=105)
 
 
-# In[6]:
+# In[7]:
 
 
 #directory must be labeled as "../dataset/training/nocrash"  
 NOCRASHDIR = '/home/jetson/ENGIN492/data_set/training/nocrash/'
 
-SWOF_nocrash_images = SWOF.generate_SWOF_data(DATADIR=NOCRASHDIR,
-                                              VIDEONAME='crash5',
-                                              frames=frames,
-                                              N=5,
-                                              startframe=28,
-                                              endframe=65)
+SWOF_nocrash_images = SWOF.generate_SWOF_data(NOCRASHDIR, 'crash5',
+                                              frames, N=5,
+                                              startframe=28, endframe=65)
 
 
-# In[ ]:
+# In[8]:
 
 
 
 #### 3. GENERATE TESTING DATASETS IN RESPECTIVE CLASS LABELS DIRECTORIES
 
 
-# In[6]:
+# In[10]:
 
 
 #directory must be labeled as "../dataset/testing/crash/"  
-# CRASHTESTDIR = '/home/jetson/ENGIN492/data_set/testing/crash/'
+CRASHTESTDIR = '/home/jetson/ENGIN492/data_set/testing/crash/'
+SWOF.generate_testdata(TRAINDIR=CRASHDIR,
+                       TESTDIR=CRASHTESTDIR,
+                       img_perc=0.1)
+ 
 
-# crash_testdata_images = SWOF.generate_testdata(DATADIR=CRASHTESTDIR,
-#  
 
-
-# In[7]:
+# In[11]:
 
 
 #directory must be labeled as "../dataset/testing/nocrash/"  
-# NOCRASHTESTDIR = '/home/jetson/ENGIN492/data_set/testing/crash/'
-
-# nocrash_testdata_images = SWOF.generate_testdata(DATADIR=NOCRASHTESTDIR,
-# 
-
-
-# In[ ]:
-
-
-
+NOCRASHTESTDIR = '/home/jetson/ENGIN492/data_set/testing/nocrash/'
+SWOF.generate_testdata(TRAINDIR=NOCRASHDIR,
+                       TESTDIR=NOCRASHTESTDIR,
+                       img_perc=0.1)
 
